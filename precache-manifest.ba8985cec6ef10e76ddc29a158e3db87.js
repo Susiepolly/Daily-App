1080d6dc3dfe3d16d93ac95c7a069015568804eb
Susiepolly <susie_poll@yahoo.co.uk>
2020-07-09 18:56:22 +0100

    Updates

---
+++ precache-manifest.ba8985cec6ef10e76ddc29a158e3db87.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1045cff905292f5907d2a2cb20be5b6",
+    "url": "/daily-app/index.html"
+  },
+  {
+    "revision": "cf9aa74e2bedb43ef05a",
+    "url": "/daily-app/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "08cb47d50d92423da91f",
+    "url": "/daily-app/static/css/main.b632579d.chunk.css"
+  },
+  {
+    "revision": "cf9aa74e2bedb43ef05a",
+    "url": "/daily-app/static/js/2.0d21bf63.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/daily-app/static/js/2.0d21bf63.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08cb47d50d92423da91f",
+    "url": "/daily-app/static/js/main.86965d76.chunk.js"
+  },
+  {
+    "revision": "e66bb6bd4d779eb1a274",
+    "url": "/daily-app/static/js/runtime-main.b1fd4979.js"
+  },
+  {
+    "revision": "d3ed2d86ae1f79cd7a3bd3176f148870",
+    "url": "/daily-app/static/media/clearnight.d3ed2d86.jpg"
+  },
+  {
+    "revision": "5c7206d9deae0a1a695e13915fad59e2",
+    "url": "/daily-app/static/media/clouds.5c7206d9.png"
+  },
+  {
+    "revision": "5b67367e21ea4c48c233921075476bb5",
+    "url": "/daily-app/static/media/cloudsN.5b67367e.png"
+  },
+  {
+    "revision": "1781d717eed3aa80f1f83f3819a84908",
+    "url": "/daily-app/static/media/fewClouds.1781d717.jpg"
+  },
+  {
+    "revision": "a128933339baf22a5f506f6c153e12fc",
+    "url": "/daily-app/static/media/fewCloudsN.a1289333.jpg"
+  },
+  {
+    "revision": "b502406a7eb7f08806e650256688cf02",
+    "url": "/daily-app/static/media/mist.b502406a.jpg"
+  },
+  {
+    "revision": "1f1938ed122e73593998001163858fc1",
+    "url": "/daily-app/static/media/rain.1f1938ed.jpg"
+  },
+  {
+    "revision": "4f770876ed9cb8d3c40861b977738a09",
+    "url": "/daily-app/static/media/rainN.4f770876.jpg"
+  },
+  {
+    "revision": "eaff34989f3869cb0b2deed01f69e874",
+    "url": "/daily-app/static/media/snow.eaff3498.jpg"
+  },
+  {
+    "revision": "eba3efa18077d55cbc34e9e8d369d373",
+    "url": "/daily-app/static/media/sun.eba3efa1.jpg"
+  },
+  {
+    "revision": "1bff3bc37fa67d330c43292202fdae75",
+    "url": "/daily-app/static/media/thunderstorm.1bff3bc3.jpg"
+  }
+]);
