275d560a1c30775f3bd7949d39e78c13dd00b24f
Susiepolly <susie_poll@yahoo.co.uk>
2020-07-09 21:54:47 +0100

    updating face feature

--- src/components/Fact.js
+++ src/components/Fact.js
@@ -1,5 +1,65 @@
 import React from 'react';
 
+function dateIndicator(date) {
+  switch(date) {
+    case 1:
+    case 21:
+    case 31:
+      return 'st';
+      break;
+    case 2:
+    case 22:
+      return 'nd';
+      break;
+    case 3:
+    case 23:
+      return 'rd';
+      break;
+    default:
+      return 'th';
+  }
+}
+
+function monthName(month) {
+  switch(month) {
+    case 1:
+      return 'January';
+      break;
+    case 2:
+        return 'February';
+        break;
+    case 3:
+      return 'March';
+      break;
+    case 4:
+      return 'April';
+      break;
+    case 5:
+      return 'May';
+      break;
+    case 6:
+      return 'June';
+      break;
+    case 7:
+      return 'July';
+      break;
+    case 8:
+      return 'August';
+      break;
+    case 9:
+      return 'September';
+      break;
+    case 10:
+      return 'October';
+      break;
+    case 11:
+      return 'November';
+      break;
+    case 12:
+      return 'December';
+      break;
+  }
+}
 
 class Fact extends React.Component {
   constructor() {
@@ -28,11 +88,12 @@ class Fact extends React.Component {
     let now = new Date(this.props.time);
     let month = now.getMonth() + 1;
     let date = now.getDate();
+    let year = this.state.fact.year;
     let dailyFact = this.state.fact.text;
 
     return(
     <div className='pa3 br3 f3-ns f4 tr w-50-ns' >
-        <p className='o-100'>{dailyFact}</p>
+        <p className='o-100'>On {date}{dateIndicator(date)} of {monthName(month)} in {year} {dailyFact}</p>
     </div>
     );
   }
